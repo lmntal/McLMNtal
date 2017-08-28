a51a6886b3d8be425d927afc4f43f00454ef7874
tsunekawa <tsunekawa@ueda.info.waseda.ac.jp>
2017-08-28 10:41:15 +0900

    Fix path to slim

--- benchmark/ctl_meta/perf.sh
+++ benchmark/ctl_meta/perf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # path to slim
-SLIM=/home/tsunekawa/lmntal/slim/bin/slim
+SLIM=slim
 
 $SLIM -p --use-builtin-rule --hl mc_ctl.lmn $1.lmn
--- benchmark/ltl_meta/perf.sh
+++ benchmark/ltl_meta/perf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # path to slim
-SLIM=/home/tsunekawa/lmntal/slim/bin/slim
+SLIM=slim
 
 $SLIM -p --use-builtin-rule --hl mc_ltl.lmn $1.lmn
